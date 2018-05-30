c9e6e4e3efc441158962875ec966d59a69faca89
John <cooks.johns@gmail.com>
2018-05-30 13:09:05 -0500

    first push

---
+++ database_ad_list/ad_seeder.sql
@@ -0,0 +1,47 @@
+CREATE table users (
+ id INT UNSIGNED NOT NULL AUTO_INCREMENT
+ password VARCHAR(25),
+ email VARCHAR(35)
+ PRIMARY KEY (id)
+);
+
+INSERT INTO users (id, email) VALUES
+  ('java@goog.com', 'securePassword'),
+  ('drBlakeDowns@childrens.hos', 'securePassword'),
+  ('meatWad@athf.as', 'securePassword'),
+  ('morty@mcFly.ld', 'securePassword'),
+  ('rick@sociopathic.lol', 'securePassword'),
+  ('hannibalBuress@ericAndreShow.as', 'securePassword'),
+  ('brak@spaceghostcoast2coast.as', 'securePassword'),
+  ('blackDebby@seaLab2021.as', 'securePassword'),
+  ('masterShaske@athf.as', 'securePassword'),
+  ('ericAndre@lateNite.com', 'securePassword'),
+  ('melissaRobbins@homeMovies.com', 'securePassword'),
+  ('john@delocated.co', 'securePassword'),
+  ('drVenture@theventure.bros', 'securePassword'),
+  ('spaceGhost@coastToCoast.as', 'securePassword'),
+  ('timHeidecker@inEverything.as', 'securePassword'),
+  ('zorak@spaceGhostCoastToCoast.as', 'securePassword'),
+  ('steveBrule@checkitOut.as', 'securePassword'),
+  ('kingFlippyNips@sunday.as', 'securePassword'),
+  ('gearHead@szechuan.mcd', 'securePassword'),
+  ('jerryboree@mortyNight.run', 'securePassword'),
+  ('kyle@theTeenyverse.as', 'securePassword'),
+  ('mrGoldenfold@jambaJuice.juc', 'securePassword'),
+  ('princeNebulon@darkMatter.as', 'securePassword'),
+  ('kingJellyBean@jellyBelly.as', 'securePassword'),
+  ('drXenonBloom@eColi.as', 'securePassword'),
+  ('annie@klumps.org', ''),
+  ('waterT@alphabetrium.exl', ''),
+  ('krombopulosMichael@assassin.lov', ''),
+  ('sleepyGary@raptor.coa', ''),
+  ('', ''),
+  ('', ''),
+  ('', ''),
+  ('', ''),
+  ('', '');
+
+
+
+
+
